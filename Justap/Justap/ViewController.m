6239b12a196bd73e33a5cf16c91c0df0ffa204c0
Mingyang <inkdot.l@gmail.com>
2014-10-04 04:01:27 -0700

    Flat Button

--- Justap/Justap/ViewController.m
+++ Justap/Justap/ViewController.m
@@ -8,20 +8,60 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "RQShineLabel.h"
 
+#import "FUIButton.h"
+#import "UIColor+FlatUI.h"
+#import "UIFont+FlatUI.h"
+
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet FUIButton *tapButton;
+@property (weak, nonatomic) IBOutlet RQShineLabel *shineLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    
+//    self.view.backgroundColor = [UIColor colorWithRed:254/255.0
+//                                                green:234/255.0
+//                                                 blue:132/255.0
+//                                                alpha:1];
+    
+    self.shineLabel.numberOfLines = 0;
+    self.shineLabel.text = @"JusTap";
+    [self.shineLabel sizeToFit];
+    self.shineLabel.textColor = [UIColor colorWithRed:9/255.0 green:112/255.0 blue:84/255.0 alpha:1];
+    self.shineLabel.center = self.view.center;
+    [self.view addSubview:self.shineLabel];
+    
+//    self.tapButton.clipsToBounds = YES;
+//    [self.tapButton setImage:[UIImage imageNamed:@"green.jpg"] forState:UIControlStateNormal];
+    
+    self.tapButton.buttonColor = [UIColor turquoiseColor];
+    self.tapButton.shadowColor = [UIColor greenSeaColor];
+    self.tapButton.shadowHeight = 10.0f;
+    self.tapButton.cornerRadius = self.tapButton.frame.size.height/2;
+//    self.tapButton.layer.borderColor = [UIColor blackColor].CGColor;
+//    self.tapButton.layer.borderWidth = 1;
+    self.tapButton.titleLabel.font = [UIFont boldFlatFontOfSize:16];
+    [self.tapButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateNormal];
+    [self.tapButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateHighlighted];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.shineLabel shine];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)tapAction:(id)sender {
 }
 
 @end
