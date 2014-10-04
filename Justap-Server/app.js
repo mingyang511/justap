fc03c2bd00b2ae142faa17fb6d3797720f5e59cf
Ashray Jain <ashrayj11@gmail.com>
2014-10-04 05:45:14 -0700

    added basic user and match handling

--- Justap-Server/app.js
+++ Justap-Server/app.js
@@ -1,3 +1,32 @@
 'use strict';
 
+var baseUrl = 'https://shining-fire-3470.firebaseio.com/';
 var Firebase = require('firebase');
+var baseFirebaseRef = new Firebase(baseUrl);
+var matchesFirebaseRef = new Firebase(baseUrl + 'matches');
+var _ = require('lodash');
+
+baseFirebaseRef.child('pool').on('value', function(snapshot) {
+	var val = snapshot.val();
+	if (val !== null) {
+		var keys = _.keys(val);
+		if (keys.length >= 2) {
+			var chosenKeys = _.sample(keys, 2);
+			var matchesValue = {};
+			_.forEach(chosenKeys, function(key) {
+				// Remove from users pool
+				console.log('Removing ' + key);
+				var urlToRemove = baseUrl + 'pool/' + key;
+				(new Firebase(urlToRemove)).remove();
+				matchesValue[key] = val[key];
+			});
+
+			// Create a new match and add both users to the match
+			var newMatchFirebaseRef = matchesFirebaseRef.push();
+			newMatchFirebaseRef.child('users').set(matchesValue);
+			newMatchFirebaseRef.child('score').set(0);
+			newMatchFirebaseRef.child('foobar').set("foo");
+		}
+	}
+});
+
