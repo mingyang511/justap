3abbf386bfd01186b6b2ce9d9132d478e2558feb
Mingyang <inkdot.l@gmail.com>
2014-10-04 02:28:33 -0700

    Setup

---
+++ Justap/Justap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Justap
+//
+//  Created by Lee on 10/4/14.
+//  Copyright (c) 2014 Mingyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Justap/Justap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Justap
+//
+//  Created by Lee on 10/4/14.
+//  Copyright (c) 2014 Mingyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
